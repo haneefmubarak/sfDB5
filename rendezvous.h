7b4d74971ba0da6cbf95a608d4d9e7b3a46dedde
Haneef Mubarak <haneefmubarak@users.noreply.github.com>
2014-07-01 16:58:49 -0700

    multilevel rendezvous

--- rendezvous.h
+++ rendezvous.h
@@ -1,22 +1,55 @@
+#pragma once
 // adapted from
 // http://git.io/pgkqvw
 // (haneefmubarak/experiments/rendezvous)
 
 #include <stdint.h>
+#include <stdlib.h>
 
 #ifndef MAX
 #define MAX(x, y) ((x) > (y) ? (x) : (y))
 #endif
 
-static inline int rendezvous (__uint128_t *pool, uint64_t id, int nodes) {
-	register int x, bin = 0;
-	register __uint128_t max = 0, cache;
+typedef struct {
+	__uint128_t val;
+	int bin;
+} rdv;
 
-	for (x = 0; x < nodes; x++) {
-		cache = pool[x] * ~id;
-		max = MAX (max, cache);
-		bin = max == cache ? x : bin;
+#define SORT_NAME rendezvous
+#define SORT_TYPE rdv
+#define SORT_CMP(x, y) ((((rdv)x).val) - (((rdv)y).val))
+#include "./deps/sort/sort.h"
+
+static inline int *rendezvous (__uint128_t *pool, uint64_t id, int nodes) {
+
+	register int x;
+
+	// store temporary results
+	rdv *cache	= malloc (nodes * sizeof (rdv));
+	if (!cache)
+		return NULL;
+	int *bin	= malloc (nodes * sizeof (int));
+	if (!bin) {
+		free (cache);
+		return NULL;
 	}
+
+	// calculate for each server
+	for (x = 0; x < nodes; x++) {
+		cache[x].val	= pool[x] * ~id;
+		cache[x].bin	= x;
+	}
+
+	// sort the results
+	rendezvous_tim_sort (cache, nodes);
+
+	// extract the results
+	for (x = 0; x < nodes; x++) {
+		bin[x] = cache[x].bin;
+	}
+
+	// cleanup
+	free (cache);
 
 	return bin;
 }
